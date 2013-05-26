014e2340ac7aba5518b30da329a5acdc78e9e488
Marcelo Rocha <contato@omarcelo.com.br>
2013-05-26 19:04:24 -0300

    Now extending TwitterBootstrapCakePHPShell class

--- Console/Command/ImportWebFilesShell.php
+++ Console/Command/ImportWebFilesShell.php
@@ -33,12 +33,13 @@
  * @version    0.8
  * @link       http://www.omarcelo.com.br
  */
+App::uses('TwitterBootstrapCakePHPShell', 'TwitterBootstrapCakePHP.Console/Command');
 /**
  * ImportWebFilesShell class
  *
  * @package TwitterBootstrapCakePHP.Console.Command
  */
-class ImportWebFilesShell extends AppShell
+class ImportWebFilesShell extends TwitterBootstrapCakePHPShell
 {
 	/**
 	 * The directory where we put the downloaded file
@@ -114,75 +115,6 @@ class ImportWebFilesShell extends AppShell
 		}
 		$this->hr();
     }
-
-    /**
-	 * Copies a file at given path, based on Shell::createFile
-	 *
-	 * @param string $source Path to the source file.
-	 * @param string $dest   The destination path.
-	 *
-	 * @return boolean Success
-	 */
-	public function copyFile($source, $dest) {
-		$source = str_replace(DS . DS, DS, $source);
-		$dest = str_replace(DS . DS, DS, $dest);
-
-		$this->out();
-
-		if (is_file($dest) ) {
-			$this->out(__d('cake_console', '<warning>File `%s` exists</warning>', $dest));
-			$key = $this->in(__d('cake_console', 'Do you want to overwrite?'), array('y', 'n', 'q'), 'n');
-
-			if (strtolower($key) === 'q') {
-				$this->out(__d('cake_console', '<error>Quitting</error>.'), 2);
-				$this->_stop();
-			} elseif (strtolower($key) !== 'y') {
-				$this->out(__d('cake_console', 'Skip `%s`', $dest), 2);
-				return false;
-			}
-		} else {
-			$this->out(__d('cake_console', 'Creating file %s', $dest));
-		}
-
-		if ( copy($source, $dest) ) {
-			$this->out(__d('cake_console', '<success>Wrote</success> `%s`', $dest));
-			return true;
-		}
-
-		$this->err(__d('cake_console', '<error>Could not write to `%s`</error>.', $dest), 2);
-		return false;
-	}
-
-	/**
-	 * Clear a directory
-	 *
-	 * @param string $path The directory path
-	 *
-	 * @return booolean
-	 */
-	protected function clearDir($path)
-	{
-		$iterator = new RecursiveIteratorIterator(
-			new RecursiveDirectoryIterator(
-				$path,
-				RecursiveDirectoryIterator::SKIP_DOTS
-			),
-		  	RecursiveIteratorIterator::CHILD_FIRST
-		);
-
-		foreach ( $iterator as $item ) {
-			if ($item->isDir()) {
-			  	if ( !rmdir($item->getRealPath())) {
-			    	$this->out(__d('cake_console', '<warning>Not able to remove the directory `%s`, please check permissions</warning>', $item));
-			    	return false;
-			    }
-		  	} elseif ( !unlink($item->getRealPath()) ) {
-			     $this->out(__d('cake_console', '<warning>Not able to remove the file `%s`, please check permissions</warning>', $item));
-			     return false;
-		  	}
-		}
-		return true;
-	}
 
 	/**
 	 * Download the "twitter bootstrap" files
